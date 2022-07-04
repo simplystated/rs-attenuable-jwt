3e3a77791d15f37da6bd57aeeba7866b565b556c
Adam Berger <adam@simplystated.dev>
2022-07-04 20:45:58 +0000

    feat: implement attenuate and seal

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use std::{borrow::Cow, str::FromStr};
 
 use base64::URL_SAFE_NO_PAD;
-use jsonwebtoken::{Header, Algorithm, EncodingKey};
-use ring::signature::{KeyPair, Ed25519KeyPair};
-use serde::{Serialize, ser::SerializeMap};
+use jsonwebtoken::{Algorithm, EncodingKey, Header};
+use ring::signature::{Ed25519KeyPair, KeyPair};
+use serde::{ser::SerializeMap, Serialize};
 use thiserror::Error;
 
 /// An AttenuableJWT carries a set of immutable claims but allows for the creation of a JWT with an attenuated
 /// set of claims.
-/// 
+///
 /// ```
 /// let ajwt = AttenuableJWT::with(jwts, key);
 /// let attenuated = ajwt.attenuate(&[("allowed-service", "something")])
@@ -20,11 +20,14 @@ pub struct AttenuableJWT<'a, KG: KeyGen> {
     private_attenuation_key: KG::Priv,
 }
 
-impl <'a> AttenuableJWT<'a, EddsaKeyGen> {
+impl<'a> AttenuableJWT<'a, EddsaKeyGen> {
     /// Constructs an AttenuableJWT from a chain of signed JWTs and a private_attenuation_key, using the default [KeyGen].
     /// Invariant: the private_attenuation_key must be the private key corresponding to the public key found in
     /// `jwts.last().unwrap().claim("aky")`.
-    pub fn with(jwts: Vec<SignedJWT>, private_attenuation_key: <EddsaKeyGen as KeyGen>::Priv) -> Self {
+    pub fn with(
+        jwts: Vec<SignedJWT>,
+        private_attenuation_key: <EddsaKeyGen as KeyGen>::Priv,
+    ) -> Self {
         Self::with_keygen(Cow::Owned(EddsaKeyGen), jwts, private_attenuation_key)
     }
 
@@ -32,7 +35,10 @@ impl <'a> AttenuableJWT<'a, EddsaKeyGen> {
     /// The `root_key` will be used to sign this initial JWT.
     /// `claims` will be augmented with an `aky` claim containing the public counterpart to the `private_attenuation_key`
     /// in the returned AttenuableJWT.
-    pub fn new<Claims: Serialize>(root_key: <EddsaKeyGen as KeyGen>::Priv, claims: Claims) -> Result<Self> {
+    pub fn new<Claims: Serialize>(
+        root_key: &<EddsaKeyGen as KeyGen>::Priv,
+        claims: Claims,
+    ) -> Result<Self> {
         Self::new_with_keygen(Cow::Owned(EddsaKeyGen), root_key, claims)
     }
 }
@@ -41,7 +47,11 @@ impl<'a, KG: KeyGen> AttenuableJWT<'a, KG> {
     /// Constructs an AttenuableJWT from a chain of signed JWTs and a private_attenuation_key, using the provided [KeyGen].
     /// Invariant: the private_attenuation_key must be the private key corresponding to the public key found in
     /// `jwts.last().unwrap().claim("aky")`.
-    pub fn with_keygen(key_gen: Cow<'a, KG>, jwts: Vec<SignedJWT>, private_attenuation_key: KG::Priv) -> Self {
+    pub fn with_keygen(
+        key_gen: Cow<'a, KG>,
+        jwts: Vec<SignedJWT>,
+        private_attenuation_key: KG::Priv,
+    ) -> Self {
         Self {
             key_gen,
             jwts,
@@ -53,7 +63,11 @@ impl<'a, KG: KeyGen> AttenuableJWT<'a, KG> {
     /// The `root_key` will be used to sign this initial JWT.
     /// `claims` will be augmented with an `aky` claim containing the public counterpart to the `private_attenuation_key`
     /// in the returned AttenuableJWT.
-    pub fn new_with_keygen<Claims: Serialize>(key_gen: Cow<'a, KG>, root_key: KG::Priv, claims: Claims) -> Result<Self> {
+    pub fn new_with_keygen<Claims: Serialize>(
+        key_gen: Cow<'a, KG>,
+        root_key: &KG::Priv,
+        claims: Claims,
+    ) -> Result<Self> {
         let (pub_key, priv_key) = key_gen.generate_key()?;
         let full_claims = FullClaims {
             user_provided_claims: claims,
@@ -70,9 +84,55 @@ impl<'a, KG: KeyGen> AttenuableJWT<'a, KG> {
         Ok(Self {
             key_gen,
             jwts: vec![SignedJWT(token)],
-            private_attenuation_key: priv_key
+            private_attenuation_key: priv_key,
         })
     }
+
+    /// Create a new AttenuableJWT that has all of the claims from this AttenuableJWT and also
+    /// carries the provided claims. When verifying, clients can ensure that the provided claims only restrict the existing
+    /// claims.
+    pub fn attenuate<Claims: Serialize>(&self, claims: Claims) -> Result<Self> {
+        let mut attenuated =
+            Self::new_with_keygen(self.key_gen.clone(), &self.private_attenuation_key, claims)?;
+        attenuated.jwts = self
+            .jwts
+            .iter()
+            .cloned()
+            .chain(attenuated.jwts.into_iter())
+            .collect();
+        Ok(attenuated)
+    }
+
+    /// Sign the full package of jwts that we have accumulated with the final attenuation key.
+    /// This creates a usable, verifiable attenuated JWT.
+    /// It contains a single claim, `jwts`, which contains an array of signed JWTs, starting from the root JWT
+    /// and ending with the most attenuated JWT.
+    /// This JWT should be verified with the key in the `aky` claim of the final JWT in `jwts`.
+    pub fn seal(&self) -> Result<SignedJWT> {
+        let header = {
+            let mut header = Header::new(Algorithm::from_str(
+                &self.private_attenuation_key.algorithm(),
+            )?);
+            header.kid = Some(self.private_attenuation_key.key_id().to_owned());
+            header
+        };
+        let claims = SealedClaims {
+            jwts: self.jwts.as_slice(),
+        };
+
+        let token = jsonwebtoken::encode(
+            &header,
+            &claims,
+            &self.private_attenuation_key.to_encoding_key()?,
+        )?;
+
+        Ok(SignedJWT(token))
+    }
+}
+
+#[derive(Serialize)]
+struct SealedClaims<'a> {
+    jwts: &'a [SignedJWT],
 }
 
 #[derive(Serialize)]
@@ -83,6 +143,8 @@ struct FullClaims<JWK: Serialize, Claims: Serialize> {
     pub aky: JWK,
 }
 
+#[derive(Clone, Serialize)]
+#[serde(transparent)]
 pub struct SignedJWT(pub String);
 
 /// A private key
@@ -93,7 +155,7 @@ pub trait PrivateKey {
 }
 
 /// A public key. The [Serialize] implementation must serialize to a JWK.
-pub trait PublicKey: Serialize { }
+pub trait PublicKey: Serialize {}
 
 pub trait KeyGen: Clone {
     type Pub: PublicKey;
@@ -110,7 +172,7 @@ impl KeyGen for EddsaKeyGen {
     type Priv = Ed25519PrivateKey;
 
     fn generate_key(&self) -> Result<(Ed25519PublicKey, Ed25519PrivateKey)> {
-        use ring::{rand::SystemRandom};
+        use ring::rand::SystemRandom;
 
         let rng = SystemRandom::new();
         let pkcs8_bytes = Ed25519KeyPair::generate_pkcs8(&rng)?;
@@ -157,16 +219,15 @@ impl PublicKey for Ed25519PublicKey {}
 
 impl Ed25519PublicKey {
     fn new(x: Vec<u8>) -> Self {
-        Self {
-            x,
-        }
+        Self { x }
     }
 }
 
 impl Serialize for Ed25519PublicKey {
     fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
     where
-        S: serde::Serializer {
+        S: serde::Serializer,
+    {
         let mut map = serializer.serialize_map(Some(3))?;
         map.serialize_entry("kty", "OKP")?;
         map.serialize_entry("crv", "Ed25519")?;
