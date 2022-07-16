60155e7e44c1efaee358619480ca1c1acc4e7dbc
Adam Berger <adam@simplystated.dev>
2022-07-16 20:49:09 +0000

    test: add a good (essentially state-machine driven) property based test

--- tests/property_tests.rs
+++ tests/property_tests.rs
@@ -1,19 +1,404 @@
 use attenuable_jwt::{
     protocol::{
-        AttenuationKeyGenerator, Issuer, PublicKey, SecondsSinceEpoch, SigningKeyManager,
-        VerificationKeyManager, VerificationRequirements,
+        AttenuationKeyGenerator, Audience, FullClaims, Issuer, PrivateKey, PublicKey, SealedClaims,
+        SecondsSinceEpoch, SignedJWT, SigningKeyManager, VerificationKeyManager,
+        VerificationRequirements,
     },
-    sign::{ed25519, AttenuableJWT, Error as SignError},
+    sign::{
+        ed25519::{self, Ed25519PublicKey},
+        AttenuableJWT, Error as SignError,
+    },
     verify::verify,
 };
-use proptest::{prelude::*, proptest};
+use mockall::mock;
+use proptest::{prelude::*, prop_oneof, proptest};
+use serde::Serialize;
 use std::{
     borrow::Cow,
     collections::HashMap,
+    iter,
+    str::FromStr,
     time::{SystemTime, UNIX_EPOCH},
 };
 
+const EXPECTED_ISSUER: &str = "expected-issuer";
+const EXPECTED_AUDIENCE: &str = "expected-audience";
+
+#[derive(Debug, Clone)]
+struct ClaimOperation {
+    claim_key: String,
+    claim_value: String,
+}
+
+#[derive(Debug, Clone)]
+enum Operation {
+    Attenuate(ClaimOperation),
+    IncorrectlyAttenuate(ClaimOperation),
+    Seal { issuer: String, audience: String },
+    SealBadIssuer,
+    SealBadExpiration,
+    SealBadNotBefore,
+    SealBadAudience,
+    SealBadSignature,
+}
+
+fn arbitrary_claim_op() -> impl Strategy<Value = ClaimOperation> {
+    any::<(String, String)>().prop_map(|(claim_key, claim_value)| ClaimOperation {
+        claim_key,
+        claim_value,
+    })
+}
+
+fn arbitrary_operation() -> impl Strategy<Value = Operation> {
+    prop_oneof![
+        arbitrary_claim_op().prop_map(Operation::Attenuate),
+        arbitrary_claim_op().prop_map(Operation::IncorrectlyAttenuate),
+        any::<(String, String)>()
+            .prop_map(|(issuer, audience)| Operation::Seal { issuer, audience }),
+        Just(Operation::SealBadIssuer),
+        Just(Operation::SealBadExpiration),
+        Just(Operation::SealBadNotBefore),
+        Just(Operation::SealBadAudience),
+        Just(Operation::SealBadSignature),
+    ]
+}
+
+fn arbitrary_operations() -> impl Strategy<Value = Vec<Operation>> {
+    proptest::collection::vec(arbitrary_operation(), 1..100)
+}
+
+mock! {
+    VerificationKeyManager {}
+
+    impl VerificationKeyManager for VerificationKeyManager {
+        type PublicRootKey = ed25519::Ed25519PublicKey;
+
+        type PublicAttenuationKey = ed25519::Ed25519PublicKey;
+
+        type PrivateAttenuationKey = ed25519::Ed25519PrivateKey;
+
+        type Claims = std::collections::HashMap<String, String>;
+
+        type JWK = ed25519::JWK;
+
+        fn get_root_key(&self, key_id: &Option<String>) -> Option<<MockVerificationKeyManager as VerificationKeyManager>::PublicRootKey>;
+        fn get_root_verification_requirements(&self) -> VerificationRequirements;
+        fn default_claims(&self) -> <MockVerificationKeyManager as VerificationKeyManager>::Claims;
+        fn jwk_to_public_attenuation_key(&self, jwk: &<MockVerificationKeyManager as VerificationKeyManager>::JWK) -> Option<<MockVerificationKeyManager as VerificationKeyManager>::PublicAttenuationKey>;
+    }
+
+    impl Clone for VerificationKeyManager {
+        fn clone(&self) -> Self;
+    }
+}
+
+fn make_verification_key_manager<Expectations>(
+    root_key: Ed25519PublicKey,
+    expectations: Expectations,
+) -> MockVerificationKeyManager
+where
+    Expectations: Fn(MockVerificationKeyManager) -> MockVerificationKeyManager
+        + Sync
+        + Send
+        + Clone
+        + 'static,
+{
+    let mut key_manager = MockVerificationKeyManager::new();
+    key_manager
+        .expect_jwk_to_public_attenuation_key()
+        .returning(|jwk| jwk.try_into().ok());
+    key_manager
+        .expect_default_claims()
+        .returning(|| Default::default());
+    let cloned_root_key = root_key.clone();
+    let cloned_expectations = expectations.clone();
+    key_manager.expect_clone().returning(move || {
+        make_verification_key_manager(cloned_root_key.clone(), cloned_expectations.clone())
+    });
+    key_manager
+        .expect_get_root_key()
+        .returning(move |_| Some(root_key.clone()));
+    expectations(key_manager)
+}
+
+fn add_expected_verifications(mut m: MockVerificationKeyManager) -> MockVerificationKeyManager {
+    m.expect_get_root_verification_requirements()
+        .returning(|| VerificationRequirements {
+            acceptable_algorithms: vec![ed25519::EDDSA_ALGORITHM.to_owned()],
+            acceptable_issuers: Some(vec![Issuer(EXPECTED_ISSUER.to_owned())]),
+            acceptable_audiences: Some(vec![Audience(EXPECTED_AUDIENCE.to_owned())]),
+            acceptable_subjects: None,
+        });
+    m
+}
+
+fn run_ops(root_claims: HashMap<String, String>, ops: Vec<Operation>) {
+    let current_time = SystemTime::now()
+        .duration_since(UNIX_EPOCH)
+        .unwrap()
+        .as_secs();
+    let key_manager = SignKeyManager;
+    let (pub_root_key, priv_root_key) = key_manager.generate_attenuation_key().unwrap();
+    let mut ajwt: AttenuableJWT<'_, SignKeyManager> = AttenuableJWT::new_with_key_manager(
+        Cow::Borrowed(&key_manager),
+        &priv_root_key,
+        root_claims.clone(),
+    )
+    .unwrap();
+
+    let mut expected_claims = root_claims;
+    let mut expect_fail = false;
+
+    let resolve_claims = |mut prev_claims: HashMap<String, String>, cur_claims| {
+        prev_claims.extend(cur_claims);
+        prev_claims
+    };
+
+    for op in ops.into_iter() {
+        match op {
+            Operation::Attenuate(ClaimOperation {
+                claim_key,
+                claim_value,
+            }) => {
+                let claims = {
+                    let mut claims = HashMap::new();
+                    claims.insert(claim_key, claim_value);
+                    claims
+                };
+                ajwt = ajwt.attenuate(claims.clone()).unwrap();
+                expected_claims = resolve_claims(expected_claims, claims);
+            }
+            Operation::IncorrectlyAttenuate(ClaimOperation {
+                claim_key,
+                claim_value,
+            }) => {
+                let jwts = ajwt.jwts();
+                let claims = {
+                    let mut claims = HashMap::new();
+                    claims.insert(claim_key, claim_value);
+                    claims
+                };
+                let (_, bad_priv_key) = key_manager.generate_attenuation_key().unwrap();
+                let (next_pub_key, next_priv_key) = key_manager.generate_attenuation_key().unwrap();
+                let next_pub_key_jwk = ed25519::JWK::from(next_pub_key);
+                let jwt = make_inner_jwt(bad_priv_key, claims, next_pub_key_jwk);
+                let jwts = jwts.iter().cloned().chain(iter::once(jwt)).collect();
+                ajwt = AttenuableJWT::with_key_manager(
+                    Cow::Borrowed(&key_manager),
+                    jwts,
+                    next_priv_key,
+                );
+                expect_fail = true;
+            }
+            Operation::Seal { issuer, audience } => {
+                let sealed = ajwt
+                    .seal(
+                        SecondsSinceEpoch(current_time + 300),
+                        SecondsSinceEpoch(current_time),
+                        Some(Issuer(issuer.clone())),
+                        Some(Audience(audience.clone())),
+                    )
+                    .unwrap();
+
+                let verification_key_manager =
+                    make_verification_key_manager(pub_root_key.clone(), move |mut m| {
+                        let iss = Issuer(issuer.clone());
+                        let aud = Audience(audience.clone());
+
+                        m.expect_get_root_verification_requirements()
+                            .returning(move || VerificationRequirements {
+                                acceptable_algorithms: vec![ed25519::EDDSA_ALGORITHM.to_owned()],
+                                acceptable_issuers: Some(vec![iss.clone()]),
+                                acceptable_audiences: Some(vec![aud.clone()]),
+                                acceptable_subjects: None,
+                            });
+                        m
+                    });
+                let verified = verify(verification_key_manager, sealed, resolve_claims);
+
+                if expect_fail {
+                    assert!(
+                        verified.is_err(),
+                        "expected verification failure, got: {:?}",
+                        &verified
+                    );
+                } else {
+                    assert!(
+                        verified.is_ok(),
+                        "expected verification success, got: {:?}",
+                        &verified
+                    );
+                    let actual_claims = verified.unwrap();
+                    assert_eq!(actual_claims, expected_claims);
+                }
+            }
+            Operation::SealBadIssuer => {
+                let exp = SecondsSinceEpoch(current_time + 300);
+                let nbf = SecondsSinceEpoch(current_time);
+                let iss = Issuer("bad-issuer".to_string());
+                let aud = Audience(EXPECTED_AUDIENCE.to_owned());
+                let token = make_envelope_jwt(
+                    ajwt.private_attenuation_key().clone(),
+                    exp,
+                    nbf,
+                    iss,
+                    aud,
+                    ajwt.jwts().iter().cloned().collect(),
+                );
+                let verification_key_manager =
+                    make_verification_key_manager(pub_root_key.clone(), add_expected_verifications);
+                let verified = verify(verification_key_manager, token, resolve_claims);
+                assert!(
+                    verified.is_err(),
+                    "expected verification failure, got: {:?}",
+                    &verified
+                );
+            }
+            Operation::SealBadExpiration => {
+                let exp = SecondsSinceEpoch(current_time - 300);
+                let nbf = SecondsSinceEpoch(current_time);
+                let iss = Issuer(EXPECTED_ISSUER.to_owned());
+                let aud = Audience(EXPECTED_AUDIENCE.to_owned());
+                let token = make_envelope_jwt(
+                    ajwt.private_attenuation_key().clone(),
+                    exp,
+                    nbf,
+                    iss,
+                    aud,
+                    ajwt.jwts().iter().cloned().collect(),
+                );
+                let verification_key_manager =
+                    make_verification_key_manager(pub_root_key.clone(), add_expected_verifications);
+                let verified = verify(verification_key_manager, token, resolve_claims);
+                assert!(
+                    verified.is_err(),
+                    "expected verification failure, got: {:?}",
+                    &verified
+                );
+            }
+            Operation::SealBadNotBefore => {
+                let exp = SecondsSinceEpoch(current_time + 300);
+                let nbf = SecondsSinceEpoch(current_time + 300);
+                let iss = Issuer(EXPECTED_ISSUER.to_owned());
+                let aud = Audience(EXPECTED_AUDIENCE.to_owned());
+                let token = make_envelope_jwt(
+                    ajwt.private_attenuation_key().clone(),
+                    exp,
+                    nbf,
+                    iss,
+                    aud,
+                    ajwt.jwts().iter().cloned().collect(),
+                );
+                let verification_key_manager =
+                    make_verification_key_manager(pub_root_key.clone(), add_expected_verifications);
+                let verified = verify(verification_key_manager, token, resolve_claims);
+                assert!(
+                    verified.is_err(),
+                    "expected verification failure, got: {:?}",
+                    &verified
+                );
+            }
+            Operation::SealBadAudience => {
+                let exp = SecondsSinceEpoch(current_time + 300);
+                let nbf = SecondsSinceEpoch(current_time);
+                let iss = Issuer(EXPECTED_ISSUER.to_owned());
+                let aud = Audience("bad-audience".to_owned());
+                let token = make_envelope_jwt(
+                    ajwt.private_attenuation_key().clone(),
+                    exp,
+                    nbf,
+                    iss,
+                    aud,
+                    ajwt.jwts().iter().cloned().collect(),
+                );
+                let verification_key_manager =
+                    make_verification_key_manager(pub_root_key.clone(), add_expected_verifications);
+                let verified = verify(verification_key_manager, token, resolve_claims);
+                assert!(
+                    verified.is_err(),
+                    "expected verification failure, got: {:?}",
+                    &verified
+                );
+            }
+            Operation::SealBadSignature => {
+                let exp = SecondsSinceEpoch(current_time + 300);
+                let nbf = SecondsSinceEpoch(current_time);
+                let iss = Issuer(EXPECTED_ISSUER.to_owned());
+                let aud = Audience(EXPECTED_AUDIENCE.to_owned());
+                let (_, bad_key) = key_manager.generate_attenuation_key().unwrap();
+                let token = make_envelope_jwt(
+                    bad_key,
+                    exp,
+                    nbf,
+                    iss,
+                    aud,
+                    ajwt.jwts().iter().cloned().collect(),
+                );
+                let verification_key_manager =
+                    make_verification_key_manager(pub_root_key.clone(), add_expected_verifications);
+                let verified = verify(verification_key_manager, token, resolve_claims);
+                assert!(
+                    verified.is_err(),
+                    "expected verification failure, got: {:?}",
+                    &verified
+                );
+            }
+        }
+    }
+}
+
+fn make_inner_jwt<SignWith: PrivateKey, NextKeyJWK: Serialize>(
+    sign_with: SignWith,
+    claims: HashMap<String, String>,
+    next_key_jwk: NextKeyJWK,
+) -> SignedJWT {
+    let header = {
+        let mut header = jsonwebtoken::Header::new(
+            jsonwebtoken::Algorithm::from_str(&sign_with.algorithm()).unwrap(),
+        );
+        header.kid = Some(sign_with.key_id().to_owned());
+        header
+    };
+    let claims: FullClaims<NextKeyJWK, HashMap<String, String>> =
+        FullClaims::new(claims, next_key_jwk);
+    let inner_jwt =
+        jsonwebtoken::encode(&header, &claims, &sign_with.to_encoding_key().unwrap()).unwrap();
+    SignedJWT(inner_jwt)
+}
+
+fn make_envelope_jwt<SignWith: PrivateKey>(
+    sign_with: SignWith,
+    exp: SecondsSinceEpoch,
+    nbf: SecondsSinceEpoch,
+    issuer: Issuer,
+    audience: Audience,
+    inner_jwts: Vec<SignedJWT>,
+) -> SignedJWT {
+    let header = {
+        let mut header = jsonwebtoken::Header::new(
+            jsonwebtoken::Algorithm::from_str(&sign_with.algorithm()).unwrap(),
+        );
+        header.kid = Some(sign_with.key_id().to_owned());
+        header
+    };
+    let full_claims = SealedClaims {
+        exp: Some(exp),
+        nbf: Some(nbf),
+        iss: Some(issuer),
+        aud: Some(audience),
+        jwts: inner_jwts,
+    };
+    let token =
+        jsonwebtoken::encode(&header, &full_claims, &sign_with.to_encoding_key().unwrap()).unwrap();
+    SignedJWT(token)
+}
+
 proptest! {
+    #[test]
+    fn verify_operations(root_claims in any::<HashMap<String, String>>(), ops in arbitrary_operations()) {
+        run_ops(root_claims, ops);
+    }
+
     #[test]
     fn sign_and_verify(issuer in any::<Option<String>>(), root_claim in any::<String>(), claims in any::<Vec<String>>()) {
         let claim_name = "my_claim";
